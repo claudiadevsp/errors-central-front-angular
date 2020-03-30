4069850144305449f42c8838c2f8c7209a68cfc3
Claudia <claudia.devsp@gmail.com>
2020-03-30 11:41:37 -0300

    [CHANGE] apis url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://errorscentralproject.herokuapp.com/api'
+  // apiUrl: 'https://errorscentralproject.herokuapp.com/api'
+  apiUrl: 'https://errors-central.herokuapp.com/api'
 };
